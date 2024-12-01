73acaa8c5ce42a1a32514fabd970858e5c7d70a9
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-12-01 10:42:32 -0300

    feat: bet version info on logo

--- resources/views/Components/app/sidebar.blade.php
+++ resources/views/Components/app/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="w-1/6 p-6 m-8 rounded-3xl sticky top-0">
     <a href="/homepage" class="text-2xl font-bold pb-8 font-poppins">
-        <span class="text-primary">Dayly</span><span class="text-secondary">Quest</span>
+        <span class="text-primary">Dayly</span><span class="text-secondary">Quest</span><span class="text-xs italic ml-1.5 font-normal">Beta</span>
     </a>
     <nav class="mt-12 flex flex-col gap-y-6 text-xl font-poppins">
         <a href="/homepage" class="{{ request()->is('homepage*') ? 'text-secondary' : 'text-white hover:text-text_gray'}} block py-2 text-lg font-poppins transition-all duration-200 ease-in-out">
