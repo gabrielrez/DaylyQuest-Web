46aca0b185acf4cd7a05305301cf39d58f5296ae
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-12-01 14:35:28 -0300

    feat: 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.layout-lp>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h1 class="font-poppins font-bold text-white text-7xl">Oops<span class="text-primary">!</span></h1>
+        <p class="font-roboto text-2xl text-text_gray mt-8">It looks like the page you're looking for has gone on vacation. 🏖️</p>
+        <p class="font-roboto text-xl text-white underline mt-5"><a href="/homepage">Back to homepage</a></p>
+    </div>
+</x-layouts.layout-lp>
