3da2b4a9557bfa907ff81a72434c048d2a5b5540
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-12-24 18:35:16 -0300

    style: mobile sidebar full width

--- resources/views/Components/app/sidebar-mobile.blade.php
+++ resources/views/Components/app/sidebar-mobile.blade.php
@@ -1,10 +1,12 @@
-<div id="side-menu" class="fixed left-0 top-0 z-40 w-72 bg-bg_black h-full border-r-2 border-bg_gray shadow-lg transform-translate-x-full transition-transform duration-300 ease-in-out">
+<div id="side-menu" class="fixed left-0 top-0 z-40 w-full bg-bg_black h-full border-r-2 border-bg_gray shadow-lg transform-translate-x-full transition-transform duration-300 ease-in-out">
     <div class="p-5">
-        <div class="flex gap-5 items-center">
-            <button id="close-menu" class="text-3xl font-bold text-right">&times;</button>
+        <div class="flex gap-5 justify-between items-center">
             <div class="text-xl font-poppins font-bold cursor-default text-center">
-                <span class="text-primary">Dayly</span><span class="text-secondary">Quest</span>
+                <a href="/homepage">
+                    <span class="text-primary">Dayly</span><span class="text-secondary">Quest</span>
+                </a>
             </div>
+            <button id="close-menu" class="text-3xl font-bold text-right">&times;</button>
         </div>
         <ul class="mt-10 flex flex-col gap-8">
             <li><a href="/homepage" class="{{ request()->is('homepage*') ? 'text-secondary' : 'text-white hover:text-text_gray'}} block text-lg font-poppins py-2"><i class="fas fa-home mr-3"></i>Home Page</a></li>
