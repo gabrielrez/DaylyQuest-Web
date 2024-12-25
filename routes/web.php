62b31e6d39074065a0e131445094fa2bf9b074f7
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-12-25 20:15:04 -0300

    fix: profile/edit route fixed

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Route::get('/login', fn() => view('auth.login'))->name('login');
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
 
-Route::get('/profile/edit', [UserController::class, 'edit']);
-
 Route::post('/support-message', [SupportController::class, 'sendMessage']);
 
 // Middleware Auth
@@ -30,43 +28,28 @@ Route::middleware(['auth', NoCache::class])->group(function () {
     Route::get('/homepage', [HomepageController::class, 'index']);
     Route::get('/settings', [SettingsController::class, 'index']);
 
+    // User
+    Route::get('/profile/edit', [UserController::class, 'edit']);
+    Route::get('/profile/{id}', [UserController::class, 'show']);
     Route::patch('/user/{id}', [UserController::class, 'update']);
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
-
-    // Profile
-    Route::prefix('profile')->group(function () {
-        Route::get('/{id}', [UserController::class, 'show']);
-    });
 
     // Collections
     Route::resource('collection', CollectionController::class)->only(['create', 'store', 'show', 'update', 'destroy']);
 
     // Goals
-    Route::prefix('goal')->middleware([CheckCollectionDeadline::class])->group(function () {
-        Route::get('/create/{collection_id}', [GoalController::class, 'create']);
-        Route::get('/steps/{goal}', [GoalController::class, 'steps']);
-        Route::post('/{collection_id}', [GoalController::class, 'store']);
-        Route::put('/complete/{goal}', [GoalController::class, 'setStatus']);
-    });
-
+    Route::get('/goal/create/{collection_id}', [GoalController::class, 'create']);
+    Route::get('/goal/steps/{goal}', [GoalController::class, 'steps']);
+    Route::post('/goal/{collection_id}', [GoalController::class, 'store']);
+    Route::put('/goal/complete/{goal}', [GoalController::class, 'setStatus']);
     Route::delete('/goal/{goal_id}', [GoalController::class, 'destroy']);
 
-    // Steps
-    Route::prefix('step')->middleware([CheckCollectionDeadline::class])->group(function () {
-        Route::post('/{goal_id}', [StepController::class, 'store']);
-        Route::put('/complete/{step}', [StepController::class, 'setStatus']);
-    });
-
-    Route::delete('/step/{step_id}', [StepController::class, 'destroy']);
-
     // Settings
-    Route::prefix('settings')->group(function () {
-        Route::get('/information', fn() => view('settings.information'));
-        Route::get('/timezone', fn() => view('settings.timezone'));
-        Route::get('/language', fn() => view('settings.language'));
-        Route::get('/account/preferences', fn() => view('settings.account-preferences'));
-        Route::get('/support', fn() => view('settings.support'));
-    });
+    Route::get('settings/information', fn() => view('settings.information'));
+    Route::get('settings/timezone', fn() => view('settings.timezone'));
+    Route::get('settings/language', fn() => view('settings.language'));
+    Route::get('settings/account/preferences', fn() => view('settings.account-preferences'));
+    Route::get('settings/support', fn() => view('settings.support'));
 
     // Community
     // Route::get('/community', [CommunityController::class, 'index']);
